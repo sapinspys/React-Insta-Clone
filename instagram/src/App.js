1aaf2b9a2a9986ea28f7b7d71393dc512d2159f3
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-18 13:34:24 -0500

    Beginning to integrate components into app

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
   constructor() {
@@ -14,6 +15,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <SearchBar />
+          {this.state.dummyData.map(post => (
+            <PostContainer />
+          ))};
         </header>
       </div>
     );
