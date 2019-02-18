e37af45dd1d2ff053d0551fecaa320eb9ccf2a9b
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-18 17:25:51 -0500

    Added new icon imgs and added some minor styling

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -7,13 +7,13 @@ function PostContainer(props) {
     return (
         <div className="post-container">
             <div className="post-user">
-                <img src={props.post.thumbnailUrl} alt="thumbnail" width='50' height='50'/>
+                <img src={props.post.thumbnailUrl} alt="thumbnail" width='40' height='40'/>
                 <p><strong>{props.post.username}</strong></p>
             </div>
             <img src={props.post.imageUrl} alt="post-image" />
             <div className="comment-section">
                 <div className="post-buttons">
-                    <img src="../../img/heart-icon.png" alt="heart"/>
+                    <img src="../../img/heart-ig.svg" alt="heart"/>
                     <img src="../../img/comment-icon.png" alt="comment"/>
                 </div>
                 <p className="post-likes"><strong>{props.post.likes} likes</strong></p>
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -5,14 +5,14 @@ function SearchBar() {
     return (
         <div className="search-container">
             <div className="search-logo">
-                <img src="../../img/ig-icon.png" alt="ig-icon"/>
-                Instagram
+                <img src="../../img/camera-ig.svg" alt="ig-icon"/>
+                <img src="../../img/ig-name-logo.png" alt="ig-logo"/>
             </div>
             <input type="text" placeholder='Search'/>
             <div className="search-icons">
-                <img src="img/compass-icon.png" alt="compass"/>
-                <img src="img/heart-icon.png" alt="heart"/>
-                <img src="img/person-icon.png" alt="person"/>
+                <img src="../../img/compass-icon.png" alt="compass"/>
+                <img src="../../img/heart-ig.png" alt="heart"/>
+                <img src="../../img/person-icon.png" alt="person"/>
             </div>
         </div>
     )
