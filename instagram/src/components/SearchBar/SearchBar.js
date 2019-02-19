8f31fe38748038dbd114ad82f3588f0d6a7b2cb2
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-18 20:02:08 -0500

    Updated navigation search colors and added magnifying glass

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -13,7 +13,7 @@ function SearchBar() {
                 <img src={igLogo} alt="ig-icon" className="ig-icon" height='25'/>
                 <img src={igNameLogo} alt="ig-logo" className="ig-logo" height='25'/>
             </div>
-            <input type="text" placeholder='Search'/>
+            <input type="text" placeholder='&#x26B2; Search'/>
             <div className="search-icons">
                 <img src={compass} alt="compass" height='25'/>
                 <img src={heart} alt="heart" height='25'/>
