f239fdd800be997bd6b646712c979676a8c92e97
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-18 20:18:35 -0500

    Added new heart icon and updated all widths to 30

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
 import './PostContainer.css';
 import moment from 'moment';
-import heart from '../../img/heart-ig.svg';
+import heart from '../../img/heart-icon.png';
 import comment from '../../img/comment-icon.png';
 import more from '../../img/more-icon.png';
 
@@ -16,8 +16,8 @@ function PostContainer(props) {
             <img src={props.post.imageUrl} className='post-image' alt="post-image" />
             <div className="comment-section">
                 <div className="post-buttons">
-                    <img src={heart} alt="heart" height='25'/>
-                    <img src={comment} alt="comment" height='25'/>
+                    <img src={heart} alt="heart" height='30'/>
+                    <img src={comment} alt="comment" height='30'/>
                 </div>
                 <p className="post-likes"><strong>{props.post.likes} likes</strong></p>
                 {props.post.comments.map((comment, index) => (
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -3,21 +3,21 @@ import './SearchBar.css';
 import igLogo from '../../img/ig-icon.png';
 import igNameLogo from '../../img/ig-name-logo.png';
 import compass from '../../img/compass-icon.png';
-import heart from '../../img/heart-ig.svg';
+import heart from '../../img/heart-icon.png';
 import person from '../../img/person-icon.png';
 
 function SearchBar() {
     return (
         <div className="search-container">
             <div className="search-logo">
-                <img src={igLogo} alt="ig-icon" className="ig-icon" height='25'/>
+                <img src={igLogo} alt="ig-icon" className="ig-icon" height='30'/>
                 <img src={igNameLogo} alt="ig-logo" className="ig-logo" height='30'/>
             </div>
             <input type="text" placeholder='&#x26B2; Search'/>
             <div className="search-icons">
-                <img src={compass} alt="compass" height='25'/>
-                <img src={heart} alt="heart" height='25'/>
-                <img src={person} alt="person" height='25'/>
+                <img src={compass} alt="compass" height='30'/>
+                <img src={heart} alt="heart" height='30'/>
+                <img src={person} alt="person" height='30'/>
             </div>
         </div>
     )
