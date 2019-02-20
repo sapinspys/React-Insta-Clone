44bb39a9d3f82300b7dc61ffaf6707e2882f1016
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-20 11:53:09 -0500

    Completed building Login class component

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -9,12 +9,32 @@ class Login extends Component {
             password=''
         }
     }
+
+    handleLogin = e => {
+        localStorage.setItem('user', this.state.username);
+        window.location.reload(); // Forces page to reload after login (un-mounts -> mounts)
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
         return (
         <div className="login-container">
-            <form onSubmit={props.onSubmit}>
-                <input type="text" placeholder='username'/>
-                <input type="text" placeholder='password'/>
+            <form onSubmit={this.handleLogin}>
+                <input type="text"
+                    name='username' 
+                    value={this.state.username} 
+                    onChange={this.handleChange} 
+                    placeholder='username'/>
+                <input type="password" 
+                    name='password' 
+                    value={this.state.password} 
+                    onChange={this.handleChange} 
+                    placeholder='password'/>
                 <button className='login-btn'>
                     Login
                 </button>
