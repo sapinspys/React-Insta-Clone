2ecef3806f20bba4b6535039624759170ad6c301
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-20 11:30:06 -0500

    Transfered authentication directory to components directory

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PostsPage from './components/PostContainer/PostsPage';
-import authenticate from './authentication/authenticate'
+import authenticate from './components/authentication/authenticate'
 
 class App extends Component {
   constructor() {
