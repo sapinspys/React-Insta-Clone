0b5c2a771dcfd6f3ad2929241e24fa209402b70f
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-20 11:36:34 -0500

    Added form, login and password inputs, and button to Login

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './Login.css';
 
 function Login() {
     return (
-        
+        <div className="login-container">
+            <form action="">
+                <input type="text"/>
+                <input type="text"/>
+                <button></button>
+            </form>
+        </div>
     )
 }
 
