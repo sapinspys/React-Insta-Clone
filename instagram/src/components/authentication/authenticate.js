bc8a0c81d627aae0b19219ef73590f6f1d8fc365
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-20 12:10:34 -0500

    Added state to inner component of authenticate

--- instagram/src/components/authentication/authenticate.js
+++ instagram/src/components/authentication/authenticate.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 const authenticate = App => LoginPage => class extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false;
+        }
+    }
+
     render() {
         return <App />
     }
