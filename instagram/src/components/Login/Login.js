bee8c53515d0dba62b18ace1259c91b95ebf0bfe
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-20 11:43:07 -0500

    Converted Login component to class component, added state, adding funcs

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Login.css';
 
-function Login() {
-    return (
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            username='',
+            password=''
+        }
+    }
+    render() {
+        return (
         <div className="login-container">
-            <form action="">
-                <input type="text"/>
-                <input type="text"/>
-                <button></button>
+            <form onSubmit={props.onSubmit}>
+                <input type="text" placeholder='username'/>
+                <input type="text" placeholder='password'/>
+                <button className='login-btn'>
+                    Login
+                </button>
             </form>
         </div>
-    )
+        )
+    }
 }
 
 export default Login;
