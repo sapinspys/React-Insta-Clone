105303b272b9828e095d3b876c35560aa3c87120
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-21 14:01:42 -0500

    Removed Login.css, no content, and marked SearchBar.css as deprecated

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import './Login.css';
-import { Button, Form, FormText} from 'reactstrap';
+import { Button, Form, FormText } from 'reactstrap';
 import styled from 'styled-components';
 import phones from '../../img/ig-login-phones.png';
 import iglogo from '../../img/ig-name-logo.png';
