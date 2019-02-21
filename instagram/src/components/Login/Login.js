1bfdf03be7e01990e60c3c1fe142ad6fc64265a3
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-21 12:42:59 -0500

    Refactored login images to Styled Components syntax

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -3,6 +3,8 @@ import './Login.css';
 import { Button, Form, FormGroup, Input, FormText} from 'reactstrap';
 import styled from 'styled-components';
 import phones from '../../img/ig-login-phones.png';
+import iglogo from '../../img/ig-name-logo.png';
+
 
 class LoginPage extends Component {
     constructor(props) {
@@ -28,9 +30,15 @@ class LoginPage extends Component {
     render() {
         return (
         <Container>
-            <img src={phones} alt="phones"/>
+            <PhonesImg></PhonesImg>
+            {/* <img src={phones} alt="phones"/> */}
             <LoginCard>
                 <Form onSubmit={this.handleLogin}>
+                    <LogoImg></LogoImg>
+                    {/* <img src={igNameLogo} alt="ig-logo" height='55'/> */}
+                    <CallToAction>
+                        Sign up to see photos and videos from your friends.
+                    </CallToAction>
                     <FormGroup>
                         <Input type="text"
                         name='username' 
@@ -63,6 +71,7 @@ const LoginCard = styled.div`
     border: 1px solid #E0E0E0;
     padding: 20px 40px;
     text-align: center;
+    background: white;
 `;
 
 const Container = styled.div`
@@ -70,6 +79,32 @@ const Container = styled.div`
     flex-direction: row;
     align-items: center;
     justify-content: center;
+    background: #F8F8F8;
+    min-height: 100vh;
+`;
+
+const CallToAction = styled.p`
+    color: #989898;
+    font-weight: 500;
+`;
+
+const PhonesImg = styled.div`
+    background-image: url(${phones});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 650px;
+    width: 380px;
+    margin-right: 15px;
+`;
+
+const LogoImg = styled.div`
+    background-image: url(${iglogo});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 55px;
+    margin: 15px 0; 
 `;
 
 export default LoginPage;
