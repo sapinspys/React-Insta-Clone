2832ce4202a15c91bededdba0d298e6c49d0bc35
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-21 14:17:17 -0500

    Began re-factoring PostContainer component using Styled  Components

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
-import './PostContainer.css';
 import moment from 'moment';
 import heart from '../../img/heart-icon.png';
 import comment from '../../img/comment-icon.png';
 import more from '../../img/more-icon.png';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 function PostContainer(props) {
     return (
-        <div className="post-container">
+        <PostCard className="post-container">
             <div className="post-user">
-                <img src={props.post.thumbnailUrl} alt="thumbnail" width='35' height='35'/>
-                <p><strong>{props.post.username}</strong></p>
+                <img src={props.post.thumbnailUrl} 
+                    alt="thumbnail" 
+                    width='35' 
+                    height='35'/>
+                <p>{props.post.username}</p>
             </div>
-            <img src={props.post.imageUrl} className='post-image' alt="post" />
+            <img src={props.post.imageUrl} 
+                className='post-image' 
+                alt="post" />
             <div className="comment-section">
                 <div className="post-buttons">
-                    <img src={heart} alt="heart" height='30' onClick={props.addLikes} />
-                    <img src={comment} alt="comment" height='30'/>
+                    <img src={heart} 
+                        alt="heart" 
+                        height='30' 
+                        onClick={props.addLikes} />
+                    <img src={comment} 
+                        alt="comment" 
+                        height='30'/>
                 </div>
-                <p className="post-likes"><strong>{props.post.likes} likes</strong></p>
+                <p className="post-likes">{props.post.likes} likes</p>
                 <CommentSection comments={props.post.comments} />
                 <p className="post-timestamp">{moment(props.post.timestamp, 'MMMM Do YYYY, h:mm:ss a').fromNow()}</p>
                 <form className="post-add-comment" data-index={props.index} onSubmit={props.commentSubmit}>
@@ -28,7 +38,7 @@ function PostContainer(props) {
                     <img src={more} alt="more" height='25'/>
                 </form>
             </div>
-        </div>
+        </PostCard>
     )
 }
 
@@ -41,5 +51,11 @@ PostContainer.propTypes = {
         comments: PropTypes.array,
     })
 }
+
+const PostCard = styled.div`
+    margin-top: 60px;
+    border: 1px solid #E0E0E0;
+    background: white;
+`;
 
 export default PostContainer;
