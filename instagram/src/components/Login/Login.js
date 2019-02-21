f3e0fbef01c9d14ea18397562adfc563f8162582
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-21 12:14:28 -0500

    Added phones image from real instagram and continued styling login page

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './Login.css';
 import { Button, Form, FormGroup, Input, FormText} from 'reactstrap';
 import styled from 'styled-components';
+import phones from '../../img/ig-login-phones.png';
 
 class LoginPage extends Component {
     constructor(props) {
@@ -26,40 +27,49 @@ class LoginPage extends Component {
 
     render() {
         return (
-        <LoginCard>
-            <Form onSubmit={this.handleLogin}>
-                <FormGroup>
-                    <Input type="text"
-                    name='username' 
-                    value={this.state.username} 
-                    onChange={this.handleChange} 
-                    placeholder='username'/>
-                </FormGroup>
-                <FormGroup>
-                    <Input type="password" 
-                    name='password' 
-                    value={this.state.password} 
-                    onChange={this.handleChange} 
-                    placeholder='password'/>
-                </FormGroup>
-                <Button className='login-btn'>
-                    Sign Up
-                </Button>
-                <FormText>
-                    By signing up, you agre to our <strong>Fake Terms</strong>, <strong>Fake Data Policy</strong>, and <strong>Fake Cookies Policy</strong>.
-                </FormText>
-            </Form>
-        </LoginCard>
+        <Container>
+            <img src={phones} alt="phones"/>
+            <LoginCard>
+                <Form onSubmit={this.handleLogin}>
+                    <FormGroup>
+                        <Input type="text"
+                        name='username' 
+                        value={this.state.username} 
+                        onChange={this.handleChange} 
+                        placeholder='username'/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Input type="password" 
+                        name='password' 
+                        value={this.state.password} 
+                        onChange={this.handleChange} 
+                        placeholder='password'/>
+                    </FormGroup>
+                    <Button className='login-btn'>
+                        Sign Up
+                    </Button>
+                    <FormText>
+                        By signing up, you agre to our <strong>Fake Terms</strong>, <strong>Fake Data Policy</strong>, and <strong>Fake Cookies Policy</strong>.
+                    </FormText>
+                </Form>
+            </LoginCard>
+        </Container>
         )
     }
 }
 
 const LoginCard = styled.div`
     width: 348px;
-    margin: 0 auto;
     border: 1px solid #E0E0E0;
     padding: 20px 40px;
     text-align: center;
+`;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
 `;
 
 export default LoginPage;
