0c9d1b2123ad689eb7ec23fd8d52d32897750d0c
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-21 13:35:57 -0500

    Re-factoring SearchBar component using Styled Components

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import './SearchBar.css';
-import igLogo from '../../img/ig-icon.png';
-import igNameLogo from '../../img/ig-name-logo.png';
+import igLogo from '../../img/ig-header-logo.png';
 import compass from '../../img/compass-icon.png';
 import heart from '../../img/heart-icon.png';
 import person from '../../img/person-icon.png';
+import styled from 'styled-components';
 
 function SearchBar(props) {
     return (
-        <div className="search-container">
+        <Header>
             <div className="search-logo">
                 <img src={igLogo} alt="ig-icon" className="ig-icon" height='30'/>
-                <img src={igNameLogo} alt="ig-logo" className="ig-logo" height='30'/>
             </div>
             <form onSubmit={props.searchSubmit}>
                 <input type="text" value={props.text} onChange={props.searchChange} placeholder='&#x26B2; Search'/>
@@ -21,8 +19,27 @@ function SearchBar(props) {
                 <img src={heart} alt="heart" height='30'/>
                 <img src={person} alt="person" height='30'/>
             </div>
-        </div>
+        </Header>
     )
 }
+
+const Header = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    height: 75px;
+    position: fixed;
+    background: white;
+    border-bottom: 1px solid #E0E0E0;
+`;
+
+const LogoHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-content: baseline;
+    width: 137px;
+    margin-left: 20px;
+`;
 
 export default SearchBar;
